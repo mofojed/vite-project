73f6077565dec8287011c74fc975830ad9ef60e5
mikebender <mikebender@deephaven.io>
2023-08-15 15:58:39 -0400

    Using the `@esbuild-plugins/node-globals-polyfill`
    - Starts up, but buffer is false

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      // Some packages need this to start properly if they reference global
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+      ],
+    },
+  },
   plugins: [react()],
-})
+});
